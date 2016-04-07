8c7fc8f7f99751463874ad89045aff1a6a483bc3
tdelubac <timothee.delubac@gmail.com>
2016-04-07 17:01:38 +0200

    adding modecoupling option to PktoXi

--- cosmo/xi.py
+++ cosmo/xi.py
@@ -3,7 +3,7 @@ Set of functions intended to study the correlation function Xi given the power s
 '''
 import numpy as np
 
-def Pk2Xi(k,Pk,rmin=0,rmax=200,nbins=200):
+def Pk2Xi(k,Pk,rmin=0,rmax=200,nbins=200,mode_coupling=False):
     '''
     Transform the 3d power spectrum to the correlation function
 
@@ -12,9 +12,10 @@ def Pk2Xi(k,Pk,rmin=0,rmax=200,nbins=200):
          Pk : 1D array containing power spectrum value at wavelength k
     
     Optional:
-         rmin  : minimum separation at which to compute the correlation function (default 0)
-         rmax  : maximum separation at which to compute the correlation function (default 200)
-         nbins : number of bins (default 200)
+         rmin          : minimum separation at which to compute the correlation function (default 0)
+         rmax          : maximum separation at which to compute the correlation function (default 200)
+         nbins         : number of bins (default 200)
+         mode_coupling : {True, False} include mode coupling form Ross et al. 2011 (eq 19) (default False)
 
     Outputs:
          r  : 1D array of distances 
@@ -27,4 +28,13 @@ def Pk2Xi(k,Pk,rmin=0,rmax=200,nbins=200):
     j0 = lambda x: [np.sin(ik * x) / (ik * x) for ik in k]
     
     Xi = [np.sum(Delta2[:-1] * j0(ir)[:-1] / k[:-1] * dk )  for ir in r]
+
+    if mode_coupling:
+        A_mc = 1.55
+        j1 = lambda x: [np.sin(ik * x) / (ik * x)**2 - np.cos(ik * x) / (ik * x) for ik in k]
+        Xi1 = [np.sum(Pk[:-1] * j1(ir)[:-1] * k[:-1] * dk) / (2*np.pi**2) for ir in r]
+        dXi = [(Xi[i+1] - Xi[i]) / (r[i+1] - r[i]) for i in np.arange(len(r[:-1]))]
+        dXi = np.append(dXi,dXi[-1])
+        Xi = [Xi[i] + A_mc * Xi1[i] * dXi[i] for i in np.arange(len(Xi))]
+        
     return r,Xi
